eb89b295d4afd522330d67c00f34cb633c665546
vuongnguyen.queanhwater@gmail.com <vuongnguyen.queanhwater@gmail.com>
2021-09-16 22:33:03 +0700

    ke thua

--- src/Cat.java
+++ src/Cat.java
@@ -1,14 +1,40 @@
-public class Cat extends Animal  {
+public class Cat extends Animal {
 
-     //extends : từ khóa kế thừa giữa class với class
+    //extends : từ khóa kế thừa giữa class với class
     // alt + enter: gợi ý sửa lỗi
-    
+
     int width;
 
-    public Cat(int height , String color , int weight, int width) {
+    public Cat(int height, String color, int weight, int width) {
         super(height, color, weight);
         this.width = width;
     }
 
+    public void run() {
+        System.out.println("Chạy bằng 4 chân");
     }
+
+//    public String info() {
+//        return "Thông tin của con Mèo " + "\n" +
+//                "Cân năng : " + weight + "\n" +
+//                "chiều cao : " + height + "\n" +
+//                "Màu lông : " + color + "\n" +
+//                "Chiều dài : " + width + "\n";
+//
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "color='" + color + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", width=" + width +
+                '}';
+    }
+}
+
+      //alt + insert : hiển thị công cự
+
+
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.security.PrivateKey;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
 
         //class & object
         //1. Class là lớp
@@ -18,12 +21,10 @@ public class Main {
 //        System.out.println("Cân năng" + cat.height);
 //        System.out.println("Cân năng" + cat.color);
 
-        Cat cat = new Cat( 1, "gray", 3, 2);
-        cat.eat();
-        System.out.println("Cân năng " + cat.weight);
-        System.out.println("Chiều cao " + cat.height);
-        System.out.println("Màu lông " + cat.color);
-        System.out.println("Chiều dài " + cat.width);
+       Cat cat = new Cat( 1, "gray", 3, 2);
+       String info = cat.toString();
+       System.out.println(info);
+
 
         // access modifier: phạm vi hoạt động
         // public , private , protected
@@ -35,9 +36,37 @@ public class Main {
 
         //́́́́́16.09/2021
         //Tính chất
-        //Tím kế thừa
+        //Tính kế thừa
         //Tính đa hình
         //Tính trườu tượng
         //Tính đóng gói  (access modifier)
+
+
     }
+
+    //1: access modifier
+    //2 : giá trả về sau khi phước thức xử lý xong
+    //3. Tên phương thức
+    // void : là kiểu dữ liệu đặc biệt không cần trả về
+
+
+    //1   2  3 => Phương thức tính tổng
+    private static int tinhTong(int a , int b){
+        int result = a + b;
+        return result;
+
+    }
+
+    public void xuly(){
+        int ketQua = tinhTong(5,10);
+        System.out.println(ketQua);
+    }
+    public void run(){
+        System.out.println(" chạy bằng 4 chân");
+    }
+
+
+
+
+
 }
