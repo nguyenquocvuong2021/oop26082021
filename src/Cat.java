f6e2122534f23975499a9468d3143bf1ec805c63
vuongnguyen.queanhwater@gmail.com <vuongnguyen.queanhwater@gmail.com>
2021-09-16 20:38:20 +0700

    kế thừa

---
+++ src/Cat.java
@@ -0,0 +1,14 @@
+public class Cat extends Animal  {
+
+     //extends : từ khóa kế thừa giữa class với class
+    // alt + enter: gợi ý sửa lỗi
+    
+    int width;
+
+    public Cat(int height , String color , int weight, int width) {
+        super(height, color, weight);
+        this.width = width;
+    }
+
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args){
 
         //class & object
@@ -11,18 +12,32 @@ public class Main {
 
         //ctrl + space : gợi ý code
         // ctrl + p : xem tham số truyền vào
-        Animal cat = new Animal(1, "gray", 3);
+//        Animal cat = new Animal(1, "gray", 3);
+//        cat.eat();
+//        System.out.println("Cân năng" + cat.weight);
+//        System.out.println("Cân năng" + cat.height);
+//        System.out.println("Cân năng" + cat.color);
+
+        Cat cat = new Cat( 1, "gray", 3, 2);
         cat.eat();
-        System.out.println("Cân năng" + cat.weight);
-        System.out.println("Cân năng" + cat.height);
-        System.out.println("Cân năng" + cat.color);
+        System.out.println("Cân năng " + cat.weight);
+        System.out.println("Chiều cao " + cat.height);
+        System.out.println("Màu lông " + cat.color);
+        System.out.println("Chiều dài " + cat.width);
 
         // access modifier: phạm vi hoạt động
-        // public , private , protected , defaut
+        // public , private , protected
         // 4 phạm vi
         // 1 - Trong class
         // 2 - ngoài class và không bởi lớp con
         // 3 - ngoài package bởi lớp con
         // 4 - trong package
+
+        //́́́́́16.09/2021
+        //Tính chất
+        //Tím kế thừa
+        //Tính đa hình
+        //Tính trườu tượng
+        //Tính đóng gói  (access modifier)
     }
 }
